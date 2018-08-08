ead577c8f9a32a817c15f88f4a29f46be826f8da
hapev <league@WTS-iMac-11.attlocal.net>
2018-08-07 18:43:08 -0700

    worked on module 4

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -1,3 +1,5 @@
+package threads;
+
 // Below is a completed program that uses brute force to crack a 9-digit password. 
 // On average, the program takes about 0.125 seconds to find the password.
 // Your goal is to re-write the algorithm using Threads. Try to shorten the time it takes 
@@ -9,29 +11,50 @@
 import java.util.Random;
 
 public class BruteForceCracker {
-	static long code = (long)(new Random().nextDouble() * 1_000_000_000);
-	
+	static long code = (long) (new Random().nextDouble() * 1_000_000_000);
+
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
-	
+	public static boolean done = false;
+
 	public static void main(String[] args) {
 		System.out.println("Starting Brute Force Checker");
 		startTime = System.currentTimeMillis();
-		
+
 		int ctr = 0;
-		while(!checkCode(ctr++));
-		
+		Thread t1 = new Thread(() -> checkRange(0, 500_000_000));
+		Thread t2 = new Thread(() -> checkRange(500_000_000, 1_000_000_000));
+
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		endTime = System.currentTimeMillis();
-		elapsedTime = (float)(endTime - startTime);
+		elapsedTime = (float) (endTime - startTime);
 		elapsedTime /= 1000.f;
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
 	}
-	
-	public static boolean checkCode(long p){
-		if(p == code){
+
+	public static void checkRange(long start, long end) {
+		long ctr = start;
+		while (!done && !checkCode(ctr++) && ctr < end)
+			;
+		if (ctr < end) {
+			done = true;
+		}
+
+	}
+
+	public static boolean checkCode(long p) {
+		if (p == code) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,36 @@
 package threads;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
 
+	public static void main(String[] args) {
+		Robot r1 = new Robot(200, 700);
+		Robot r2 = new Robot(300, 700);
+		Robot r3 = new Robot(400, 700);
+		Robot r4 = new Robot(500, 700);
+		Robot r5 = new Robot(600, 700);
+		Thread t1 = new Thread(() -> circle(r1));
+		Thread t2 = new Thread(() -> circle(r2));
+		Thread t3 = new Thread(() -> circle(r3));
+		Thread t4 = new Thread(() -> circle(r4));
+		Thread t5 = new Thread(() -> circle(r5));
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+	}
+
+	private static void circle(Robot r1) {
+		r1.penDown();
+		for (int i = 0; i < 365; i++) {
+			r1.turn(1);
+			r1.move(1);
+
+		}
+		r1.hide();
+	}
 }
-
